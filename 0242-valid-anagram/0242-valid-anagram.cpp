fdf17be98b6a49ddf5046f13571c52337b3dba03
Shreyansh Arora <62873436+shreyansh012@users.noreply.github.com>
2022-10-20 23:11:23 +0530

    Time: 14 ms (71.32%), Space: 7.2 MB (96.75%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())
+            return false;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        //cout<<s<<endl<<t;
+        for(int i=0;i<s.length();i++){
+            if(s[i]!=t[i])
+                return false;
+        }
+        return true;
+    }
+};
