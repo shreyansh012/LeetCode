27b45f54430c5cecf913430c44c9728a8c652dd5
Shreyansh Arora <62873436+shreyansh012@users.noreply.github.com>
2022-10-22 12:43:31 +0530

    Time: 3 ms (81.02%), Space: 6.5 MB (76.30%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        int arr[26]={0};
+        for(int i=0;i<sentence.length();i++){
+            arr[sentence[i]-97]++;
+        }
+        for(int i=0;i<26;i++){
+            if(arr[i]==0)
+                return false;
+        }
+        return true;
+    }
+};
