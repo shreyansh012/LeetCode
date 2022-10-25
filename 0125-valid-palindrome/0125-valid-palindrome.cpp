24fad7f1b16cde46ac497ff5c40bb9fdb3207233
Shreyansh Arora <62873436+shreyansh012@users.noreply.github.com>
2022-10-25 11:42:29 +0530

    Time: 22 ms (12.10%), Space: 7.4 MB (56.44%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        
-        // for(int i=0;i<s.length();i++){
-        //     if(s[i]>='A' && s[i]<='Z')
-        //         s[i]+=32;
-        // }
-        // int i=s.length()-1;
-        // while(i){
-        //     if((s[i]<'a'|| s[i]>'z') && (s[i]<'0' || s[i]>'9'))
-        //         s.erase(i,1);
-        //         i--;
-        // }
-        // int j=0,k=s.length()-1;
-        // while(j<k){
-        //     if(tolower(s[j])!=tolower(s[k]))
-        //         return false;
-        // }
-        int i=0,j=s.length()-1;
-        while(i<j){
-            if((s[i]>='a'&& s[i]<='z') || (s[i]>='0' && s[i]<='9') || (s[i]>='A' && s[i]<='Z')){
-                if((s[j]>='a'&& s[j]<='z') || (s[j]>='0' && s[j]<='9') || (s[j]>='A' && s[j]<='Z')){
-                    if(tolower(s[i])!=tolower(s[j]))
-                        return false;
-                    else i++;j--;
-                }
-                else
-                    j--;
+        int st=0,e=s.size()-1;
+    while(st<=e){
+        if(isalnum(s[st]) && isalnum(s[e])){
+            //cout<<s[st]<< " & "<<s[e]<<endl;
+            if(tolower(s[st])!=tolower(s[e]))
+                return false;
+            else {
+                st++;e--;
             }
-            else i++;
         }
+        else if(isalnum(s[st])){
+            e--;
+        }
+        else if(isalnum(s[e])){
+            st++;
+        }
+        else {st++;e--;}
             
-        return true;
+    }
+    return true;
     }
 };
