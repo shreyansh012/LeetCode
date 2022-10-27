8ba4896f3a45b55017d75a10348c28bdf9662888
Shreyansh Arora <62873436+shreyansh012@users.noreply.github.com>
2022-10-28 01:42:47 +0530

    Time: 3 ms (65.48%), Space: 6 MB (38.20%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1)
-            return n;
-        
-        return fib(n-1)+fib(n-2);
-        
+        int start=0;
+        int second=1;
+        for(int i=0;i<n;i++){
+            int sum=second;
+            second+=start;
+            start=sum;
+        }
+        return start;
     }
 };
