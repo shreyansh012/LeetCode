f833ccd8e2a7c76cf5ff223c1b9237f62d312585
Shreyansh Arora <62873436+shreyansh012@users.noreply.github.com>
2022-10-31 21:14:25 +0530

    Time: 44 ms (8.41%), Space: 17.5 MB (15.16%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -4,7 +4,7 @@ public:
         int m=matrix.size();
         int n=matrix[0].size();
         
-        for(int i=0;i<m;i++){
+        for(int i=0;i<m-1;i++){
             for(int j=0;j<n-1;j++){
                 int r=i+1,c=j+1;
                 while(r<m && c<n){
