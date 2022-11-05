9534df90716f6489fa5aa66a725b5a34fcd9fbaf
Shreyansh Arora <62873436+shreyansh012@users.noreply.github.com>
2022-11-06 00:21:18 +0530

    Time: 40 ms (28.63%), Space: 10.8 MB (27.77%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        if(numRows <= 1) return s;
-        vector<string> db (numRows, "");
-        for(int i = 0,row = 0,step = 1;i < s.size();i++){
-            db[row] += s[i];
-            if(row == 0) step = 1;
-            if(row == numRows - 1) step = -1;
-            row += step;
+        vector<string> st(numRows,"");
+        int i=0;
+        int n=s.size();
+        while(i<n){
+            for(int j=0;j<numRows && i<n;j++){
+                st[j].push_back(s[i++]);
+            }
+            for(int k=numRows-2;k>=1 && i<n;k--){
+                st[k].push_back(s[i++]);
+            }
         }
-        string ret;
-        for(auto d:db) ret+=d;
-        return ret;
+        string ans="";
+        for(auto i:st){
+            ans+=i;
+        }
+        return ans;
     }
 };
