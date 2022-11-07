3b9687e239f8d063cb3844c32dcbd4056dec2d5e
Shreyansh Arora <62873436+shreyansh012@users.noreply.github.com>
2022-11-07 18:24:23 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (97.56%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        int n=num;
-        int digits=-1;
-        while(n){
-            digits++;
-            n/=10;
-        }
         
-        int mult=pow(10,digits);
-        int carry=0;
-        n=num;
-        while(mult){
-            if((n/mult)==6){
-                n+=(mult*3);
-                    return carry+n;
-            }
-            else{
-             carry+=(9*mult);
-                n=n-(9*mult);
-                mult/=10;
-            }
+    string s=to_string(num);
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='6'){
+            s[i]='9';
+            break;
         }
-       return num; 
+    }
+    num=stoi(s);
+    return num;
     }
 };
