8f2b0cc05d4b5030c2dbd6a7d599d2e1c5f6c9ef
Shreyansh Arora <62873436+shreyansh012@users.noreply.github.com>
2022-11-17 22:15:55 +0530

    Time: 24 ms (18.00%), Space: 5.8 MB (75.43%) - LeetHub

---
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int area1,area2;
+        area1=(ax2-ax1)*(ay2-ay1);
+        area2=(bx2-bx1)*(by2-by1);
+        
+        int area3=0;
+        if(bx1<=ax2 && bx2>=ax1 && by1<=ay2 && by2>=ay1){
+           int l=min(ax2,bx2)-max(ax1,bx1);
+            int b=min(ay2,by2)-max(ay1,by1);
+            area3=l*b;
+        }
+        return area1+area2-area3;
+    }
+};
