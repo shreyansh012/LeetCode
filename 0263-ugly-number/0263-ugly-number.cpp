947badb489ea57f08fa431398191a9dd208ad039
Shreyansh Arora <62873436+shreyansh012@users.noreply.github.com>
2022-11-18 22:42:48 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (86.72%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n==0)
+            return false;
+        if(n==1)
+            return true;
+        
+        int i=2;
+        while(i<=5){
+            while(n%i==0){
+                n/=i;
+            }
+            i++;
+        }
+        if(n==1)
+            return true;
+        else return false;
+    }
+};
