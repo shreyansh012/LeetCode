7c176d324a0b7b9406be98cd9e742f4ae733d39b
Shreyansh Arora <62873436+shreyansh012@users.noreply.github.com>
2022-11-19 13:35:58 +0530

    Time: 0 ms (100.00%), Space: 6 MB (70.09%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
@@ -8,15 +8,18 @@ public:
         }
         int minsum=0;
         sort(digits.begin(),digits.end());
-        string s1,s2,s3,s4;
-        s1=to_string(digits[0]);
-        s4=to_string(digits[3]);
-        s3=to_string(digits[2]);
-        s2=to_string(digits[1]);
-        s1+=s4;
-        s2+=s3;
-        int n1=stoi(s1);
-        int n2=stoi(s2);
+        // string s1,s2,s3,s4;
+        // s1=to_string(digits[0]);
+        // s4=to_string(digits[3]);
+        // s3=to_string(digits[2]);
+        // s2=to_string(digits[1]);
+        // s1+=s4;
+        // s2+=s3;
+        // int n1=stoi(s1);
+        // int n2=stoi(s2);
+        int n1,n2;
+        n1=digits[0]*10+digits[3];
+        n2=digits[1]*10+digits[2];
         return n1+n2;
     }
 };
