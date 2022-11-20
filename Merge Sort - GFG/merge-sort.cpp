cfa4655fe2f760b0cdcc5a18592f147023ea8db8
Shreyansh Arora <62873436+shreyansh012@users.noreply.github.com>
2022-11-21 01:24:29 +0530

    Added solution - LeetHub

--- Merge Sort - GFG/merge-sort.cpp
+++ Merge Sort - GFG/merge-sort.cpp
@@ -19,48 +19,50 @@ void printArray(int arr[], int size)
 class Solution
 {
     public:
-    void merge(int arr[],int s,int e){
-    int mid=s+((e-s)>>1);
-    int l1=mid-s+1;
-    int l2=e-mid;
-
-    int arr1[l1],arr2[l2];
-    int k=s;
-    for(int i=0;i<l1;i++){
-        arr1[i]=arr[k++];
+    void merge(int arr[], int s, int mid, int e)
+    {
+         // Your code here
+         int l1=mid-s+1;
+         int l2=e-mid;
+         
+         int arr1[l1],arr2[l2];
+         int k=s;
+         for(int i=0;i<l1;i++){
+             arr1[i]=arr[k++];
+         }
+         for(int i=0;i<l2;i++){
+             arr2[i]=arr[k++];
+         }
+         int i=0,j=0;
+         k=s;
+         while(i<l1 && j<l2){
+             if(arr1[i]<arr2[j])
+                arr[k++]=arr1[i++];
+             else 
+                arr[k++]=arr2[j++];
+         }
+         while(i<l1){
+             arr[k++]=arr1[i++];
+         }
+         while(j<l2){
+             arr[k++]=arr2[j++];
+         }
     }
-    for(int i=0;i<l2;i++){
-        arr2[i]=arr[k++];
+    public:
+    void mergeSort(int arr[], int s, int e)
+    {
+        //code here
+        if(s>=e)
+            return;
+            
+        int mid=s+((e-s)/2);
+        //left hand side call
+        mergeSort(arr,s,mid);
+        //rigth hand side call
+        mergeSort(arr,mid+1,e);
+        //merge both
+        merge(arr,s,mid,e);
     }
-
-    int i=0,j=0;
-    k=s;
-    while(i<l1 && j<l2){
-        if(arr1[i]<arr2[j])
-            arr[k++]=arr1[i++];
-        else 
-            arr[k++]=arr2[j++];
-    }
-
-    while(i<l1)
-        arr[k++]=arr1[i++];
-    
-    while(i<l2)
-        arr[k++]=arr2[j++];
-
-}
-
-void mergeSort(int *arr,int s,int e){
-    if(s>=e)
-        return;
-    int mid=s+((e-s)>>1);
-    //left half
-    mergeSort(arr,s,mid);
-    //right half
-    mergeSort(arr,mid+1,e);
-    //merge both
-    merge(arr,s,e);
-}
 };
 
 //{ Driver Code Starts.
