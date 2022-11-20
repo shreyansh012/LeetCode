07ab1ec6043a9f864899d9cc9899bfe4ac891634
Shreyansh Arora <62873436+shreyansh012@users.noreply.github.com>
2022-11-20 20:57:26 +0530

    Time: 3 ms (87.28%), Space: 5.9 MB (26.02%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int ctr=0;
-        for(int i=1;i<=n;i++){
-            ctr++;
-            n-=i;
-        }
-        return ctr;
+        return int(sqrt(1 + 8l*n)-1) >> 1;
     }
 };
