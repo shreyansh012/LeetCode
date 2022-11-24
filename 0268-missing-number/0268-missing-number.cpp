9dacb3da550f5a5121bb691d9fab9718a3f0a9e5
Shreyansh Arora <62873436+shreyansh012@users.noreply.github.com>
2022-11-24 23:22:54 +0530

    Time: 53 ms (36.89%), Space: 18.1 MB (21.82%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,10 +3,9 @@ public:
     int missingNumber(vector<int>& nums) {
         int unique=0;
         int n=nums.size();
-        for(int i=0;i<=n;i++){
-            unique=unique^i;
-        }
+        int num=1;
         for(int i=0;i<n;i++){
+            unique=unique^num++;
             unique=unique^nums[i];
         }
         return unique;
