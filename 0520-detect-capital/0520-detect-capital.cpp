caad043f7b28adfe5ac0ff9d18527c528cad2d1b
Shreyansh Arora <62873436+shreyansh012@users.noreply.github.com>
2023-01-03 00:06:33 +0530

    Time: 9 ms (7.51%), Space: 6.1 MB (24.57%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -2,19 +2,13 @@ class Solution {
 public:
     bool detectCapitalUse(string word) {
         int n=word.length();
-        if(isupper(word[0]) && islower(word[1])){
+        if(isupper(word[0]) && isupper(word[1])){
             for(int j=2;j<n;j++){
-                if(isupper(word[j]))
+                if(islower (word[j]))
                     return false;
             }
         }
-        else if(isupper(word[0])){
-            for(int j=1;j<n;j++){
-                if(!isupper(word[j]))
-                    return false;
-            }
-        }
-        else if(islower(word[0])){
+        else{
             for(int j=1;j<n;j++){
                 if(!islower(word[j]))
                     return false;
